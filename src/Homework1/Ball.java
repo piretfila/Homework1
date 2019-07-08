7006f9c228447653321d30cce22f74b9483b4f28
Joonas <joonas.ervald@gmail.com>
2019-07-08 18:51:26 +0300

    Define what a Ball is

--- src/Homework1/Ball.java
+++ src/Homework1/Ball.java
@@ -1,14 +1,35 @@
 package Homework1;
 
+import java.util.Random;
+
 public class Ball {
 
-    // Data related to a ball
-    public String color;
-
-    public Ball() {
-        // Creating a ball
-        color = "R";
+    // Define color
+    enum Color {
+        Green,
+        Blue,
+        Red
     }
 
+    // Data related to a ball
+    public Color color;
 
+    public Ball() {
+        // Randomize a number from 0 to 2
+        Random randomiser = new Random();
+        int randomNumber = randomiser.nextInt(3);
+
+        // Decide which color the ball will be
+        if (randomNumber == 0) {
+            color = Color.Red;
+        } else if (randomNumber == 1) {
+            color = Color.Blue;
+        } else {
+            color = Color.Green;
+        }
+    }
+
+    public void changeColor(Color newColor) {
+        color = newColor;
+    }
 }
--- src/Homework1/Homework1.java
+++ src/Homework1/Homework1.java
@@ -5,8 +5,9 @@ public class Homework1 {
 
     public static void main(String[] args) {
         Ball punanePall = new Ball();
+        punanePall.changeColor(Ball.Color.Red);
         Ball sininePall = new Ball();
-        sininePall.color = "B";
+        sininePall.changeColor(Ball.Color.Blue);
 
         System.out.println(punanePall.color);
         System.out.println(sininePall.color);
