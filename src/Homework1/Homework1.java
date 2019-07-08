22255399608dfe2280835f82ec8889e1af296d45
Joonas <joonas.ervald@gmail.com>
2019-07-08 18:26:33 +0300

    Class and method intro

--- src/Homework1/Homework1.java
+++ src/Homework1/Homework1.java
@@ -2,7 +2,23 @@ package Homework1;
 
 public class Homework1 {
 
+
     public static void main(String[] args) {
-        // do something here...
+        sayHello();
+        sayHello();
+        sayGoodbye();
+        System.out.println(getName();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Bye");
+    }
+
+    public static String getName() {
+        return "Piret";
     }
 }
